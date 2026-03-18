43f72ebd0eddc08e60f5b4dfb3ef06f557ce7420
Pedro Galvão <90785456+Ordep-42@users.noreply.github.com>
2026-03-18 11:24:03 -0300

    feat: Buttons to freeze/unfreeze display and clear display

--- src/oled_display.c
+++ src/oled_display.c
@@ -13,9 +13,14 @@
 #define OLED_I2C_FREQ 400000
 #define OLED_ADDR 0x3C
 
+#define BTN_A_PIN 6
+#define BTN_B_PIN 7
+
 #define LINE_BUF_SIZE 64
 #define MAX_LINES 7
 #define OLED_QUEUE_SIZE 256
+
+static volatile bool display_frozen = false;
 
 static ssd1306_t display;
 static QueueHandle_t oled_byte_queue;
@@ -27,12 +32,49 @@ static void oled_scroll_println(const char *line) {
         strncpy(lines[i], lines[i + 1], LINE_BUF_SIZE - 1);
     strncpy(lines[MAX_LINES - 1], line, LINE_BUF_SIZE - 1);
 
-    ssd1306_clear(&display);
-    for (int i = 0; i < MAX_LINES; i++) {
-        if (lines[i][0] != '\0')
-            ssd1306_draw_string(&display, 0, i * 9, 1, lines[i]);
-    }
-    ssd1306_show(&display);
+	if (!display_frozen) {
+	    ssd1306_clear(&display);
+	    for (int i = 0; i < MAX_LINES; i++) {
+	        if (lines[i][0] != '\0')
+	            ssd1306_draw_string(&display, 0, i * 9, 1, lines[i]);
+	    }
+	    ssd1306_show(&display);
+	}
+}
+
+static void buttons_task(void *ptr) {
+	bool btn_a_last = true;
+	bool btn_b_last = true;
+
+	while (1) {
+		bool btn_a = gpio_get(BTN_A_PIN);
+		bool btn_b = gpio_get(BTN_B_PIN);
+
+		if (!btn_a && btn_a_last) {
+			for (int i = 0; i < MAX_LINES; i++)
+				lines[i][0] = '\0';
+			ssd1306_clear(&display);
+			ssd1306_show(&display);
+		}
+
+		if (!btn_b && btn_b_last) {
+			display_frozen = !display_frozen;
+
+			if (!display_frozen) {
+				ssd1306_clear(&display);
+				for (int i = 0; i < MAX_LINES; i++) {
+					if (lines[i][0] != '\0')
+						ssd1306_draw_string(&display, 0, i*9, 1, lines[i]);
+				}
+				ssd1306_show(&display);
+			}
+		}
+
+		btn_a_last = btn_a;
+		btn_b_last = btn_b;
+
+		vTaskDelay(pdMS_TO_TICKS(50));
+	}
 }
 
 // Task dedicada — única função que toca o I2C/display
@@ -64,6 +106,14 @@ static void oled_task(void *ptr) {
 }
 
 void oled_display_init(void) {
+	gpio_init(BTN_A_PIN);
+	gpio_set_dir(BTN_A_PIN, GPIO_IN);
+	gpio_pull_up(BTN_A_PIN);
+
+	gpio_init(BTN_B_PIN);
+	gpio_set_dir(BTN_B_PIN, GPIO_IN);
+	gpio_pull_up(BTN_B_PIN);
+
     i2c_init(OLED_I2C_PORT, OLED_I2C_FREQ);
     gpio_set_function(OLED_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(OLED_SCL_PIN, GPIO_FUNC_I2C);
@@ -80,6 +130,7 @@ void oled_display_init(void) {
 
 void oled_task_create(void) {
     xTaskCreate(oled_task, "OLED", 512, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(buttons_task, "BTTNS", 256, NULL, tskIDLE_PRIORITY + 1, NULL);
 }
 
 // Chamado do cdc_task — apenas enfileira, nunca bloqueia
